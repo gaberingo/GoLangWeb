19955c83cb97c9a7be1c5a88caceedeb308bbfca
gaberingo <iotkugabe@gmail.com>
2024-02-29 00:22:22 +0700

    [UPD] create function to access database mysql

--- main.go
+++ main.go
@@ -1,48 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"ringo_web/config"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "Error 404 not found.", http.StatusNotFound)
-		return
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, "Error 405 method not allowed.", http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Fprintf(w, "Hello!")
-}
-
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "POST request successful\n")
-	name := r.FormValue("name")
-	email := r.FormValue("email")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Email = %s\n", email)
-}
-
 func main() {
-	fmt.Println("Hello, World!")
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
-
-	fmt.Println("Starting server at port 8000")
-
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	config.ConnectDB()
+	log.Println("Server running on port 8000")
+	http.ListenAndServe(":8000", nil)
 }
